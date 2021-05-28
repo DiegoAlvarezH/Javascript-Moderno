231209ace08f6f77cec1a43ceb0539746ee425c3
Diego Alvarez <diegoah905@gmail.com>
2021-05-27 23:30:06 -0500

    archivo para hablar de palabtas reservadas en javascript

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,14 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Lexical_grammar
+
+//calcase sensitive 
+//uso especifico de palabras
+
+//let constante = 123;
+
+// variavles que comiencen con numeros, ejemplo - let 1abc = 1;
+
+//let objeto$ = 12;
+
+// los puntos son para acceder a una propiedad o metodo.
+
+//guiones bajos _ se pueden usar

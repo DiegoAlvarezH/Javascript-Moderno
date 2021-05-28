03665bd620327591e6e7821526a12650d5c59858
Diego Alvarez <diegoah905@gmail.com>
2021-05-28 00:37:30 -0500

    explicando los arreglos y como acceder

---
+++ 01-fundamentos/assets/js/arreglos.js
@@ -0,0 +1,35 @@
+//arreglos; son un objeto muy parecido a una lista de informacion, que contienen un grupo de elemento
+
+//const arr = new Array(10);
+
+//const arr = []
+
+let colores = [ 'rojo', 'verde', 'amarillo'];
+console.log({ colores });
+
+console.log( colores[1]);
+
+let arregloCosas = [
+    true,
+    1234,
+    'Diego',
+    1 + 2,
+    function(){},
+    ()=>{},
+    { a: 1 },
+    ['x', 'megaman', 'zero', 'dr ligth', [
+        'dr w',
+        'woodman'
+    ]]
+];
+
+// console.log(arregloCosas[7][0]);
+
+console.log(arregloCosas[7][4][0]);
+
+
+
+
+
+
+ 

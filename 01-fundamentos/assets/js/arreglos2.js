ab1be5e37bb001c70621ed14a5d5d9f1ef8abccb
Diego Alvarez <diegoah905@gmail.com>
2021-05-29 19:39:19 -0500

    hablando un poco de los arreglos

---
+++ 01-fundamentos/assets/js/arreglos2.js
@@ -0,0 +1,9 @@
+//los metodos son funciones que vienen internas de los arreglos
+//y las propiedades son una variable que vienen en los arreglos
+
+let juego = [
+    'zelda',
+    'mario',
+    'chrono',
+    'battlefield'
+]

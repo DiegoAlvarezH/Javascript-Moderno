a0fcbc82bf415a689046ba223d490c26ccd8083b
Diego Alvarez <diegoah905@gmail.com>
2021-05-30 23:58:12 -0500

    explicando el metodo .forEach

--- 01-fundamentos/assets/js/arreglos2.js
+++ 01-fundamentos/assets/js/arreglos2.js
@@ -5,11 +5,19 @@ let juego = [
     'zelda',
     'mario',
     'chrono',
-    'battlefield'
-]
+    'battlefield',
+    123
+];
 
 console.log('Largo:', juego.length);
 
-let primero = juego[0]
+let primero = juego[3 -3 ]
 
-let ultimo = juego[3];
+let ultimo = juego[juego.length - 1]; 
+
+console.log({primer, ultimo});
+
+//el  forEach va a ejecutar una accion independiente, segun cada posicion del arreglo
+juego.forEach((elemento, indice, arr) => {
+    console.log({elemento, indice, err});
+});

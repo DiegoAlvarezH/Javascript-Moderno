826020162688339f8467a2c434c106040e3eed83
Diego Alvarez <diegoah905@gmail.com>
2021-05-31 23:48:32 -0500

    terminando con los arreglos por ahora

--- 01-fundamentos/assets/js/arreglos2.js
+++ 01-fundamentos/assets/js/arreglos2.js
@@ -5,23 +5,46 @@ let juego = [
     'zelda',
     'mario',
     'chrono',
-    'battlefield',
-    123
+    'battlefield'
 ];
 
 console.log('Largo:', juego.length);
 
-let primero = juego[3 -3 ]
+let primero = juego[3 - 3]
 
 let ultimo = juego[juego.length - 1]; 
 
-console.log({primer, ultimo});
+console.log({primero, ultimo});
 
 //el  forEach va a ejecutar una accion independiente, segun cada posicion del arreglo
 juego.forEach((elemento, indice, arr) => {
-    console.log({elemento, indice, err});
+    console.log({elemento, indice, arr});
 });
 
-//el metodo .push para agregar a nuestro array 
+//el metodo .push para agregar a nuestro array *al final*
+//el metodo es una funcion que se va a ejecutar cuando nosotros le indiquemos
 
-juego.push( 'Mortal kombat' );
+let nuevojuego = juego.push( 'Mortal kombat' );
+console.log({nuevojuego, juego});
+
+//el metodo para agregar al principio de nuestro array(arreglo) es .unshift()
+
+juegodemoda = juego.unshift('fail guys');
+console.log({juegodemoda, juego});
+
+//el metodo para eliminar el primer elemento del array(arreglo) es .pop
+let juegoborrado = juego.pop();
+console.log({ juegoborrado, juego});
+
+//eliminar un elemento del array en una posicion especifica
+
+let pos = 1;
+
+console.log({juego});
+let juegoBorrado = juego.splice( pos, 2);
+console.log({ juegoBorrado, juego});
+
+//buscar un valor del array (este retorna la posicion)
+
+let metroidIndex = juego.indexOf('zelda');
+console.log({ metroidIndex });

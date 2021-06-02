c90893c28d9d9267558952954bf0445c65c0bf1b
Diego Alvarez <diegoah905@gmail.com>
2021-06-02 02:32:17 -0500

    comenzando con las funciones

---
+++ 01-fundamentos/assets/js/funciones.js
@@ -0,0 +1,35 @@
+//una funcion es una serie de instrucciones que podemos ejecutar y reutilizar en nuestro codigo
+//termino tecnico = es centralizar la logica de un procedimiento que podemos reutilizar varias veces
+
+//un metodo es una funcion que se encuentra dentro de un objeto
+
+
+function saludar(nombre){
+    console.log('Hola ' + nombre);
+}
+
+const saludar2 = function(nombre){
+    console.log('Hola ' + nombre);
+}
+
+const saludarFlecha = () => {
+    console.log('Hola flecha');
+}
+ 
+const saludarFlecha2 = (nombre) => {
+    console.log('Hola flecha' + nombre);
+}
+
+
+//funcion anonima no tiene nombre pero se asigna
+// const saludar2 = function(){
+//     console.log('hola mundo');
+// }
+
+
+saludar('Diego', 22, true, 'Colombia'); 
+// saludar2('Diego')
+saludarFlecha2('Alejo');
+
+
+//llamar con ();
--- 01-fundamentos/assets/js/objeto-literal.js
+++ 01-fundamentos/assets/js/objeto-literal.js
@@ -2,7 +2,7 @@
 //estos tienen una propiedad {} y para asignar :
 //no tiene limite, se pueden anidar
 //utiliza camelcase
-let personaje = {
+const personaje = {
     nombre: 'Tony Stark',
     codeName: 'Ironman',
     vivo: false,
@@ -15,7 +15,8 @@ let personaje = {
     direccion: {
         zip: '10880, 90265', 
         ubicacion: 'Malibu, CA'
-    }
+    },
+    'Ultima-pelicula': 'Infinity war'
 };
 //imprime en orden alfabetico, pero VScode lo puede arreglar tambien si a la hora de escribir se te olvida
 console.log(personaje);
@@ -27,3 +28,34 @@ console.log('Coords', personaje.coords);
 console.log('Lat', personaje.coords.lat);
 
 console.log('No. Trajes', personaje.trajes.length);
+//para obtener el ultimo traje, asi se agreguen otros   
+console.log('Ultimo traje', personaje.trajes[personaje.trajes.length - 1]);
+
+const x = 'vivo';
+console.log('Vivo', personaje.vivo);
+
+//cuando queremos ultilizar un espacio o un guion, lo ideal es que lo hagamos con comillas simples 
+console.log('Ultima pelicula', personaje['Ultima-pelicula']);
+
+//mas detalles 
+//borrar propiedad se utiliza la propiedad delete
+delete personaje.edad 
+
+console.log(personaje);
+
+personaje.casado = true;
+
+const entriesPares = Object.entries(personaje);
+console.log( entriesPares );
+
+//metodo para congelar lo que tenga, es decir que es inmutable(bloqueado) que no se puede cambiar, a la hora de yo ejecutar dicho metodo.
+
+Object.freeze(personaje)
+
+//pero este no congela las propiedades de los objetos que estan dentro del objeto padre, o muy bien pasarle el freeze a dicha propiedad
+personaje.dinero = 1000000000;
+personaje.direccion.ubicacion = 'Costa rica';
+
+const propiedades = Object.getOwnPropertyNames(personaje);
+const valores = Object.values(personaje);
+console.log({propiedades, valores});

71aa363868fc5fe43798408ba8263ab096868019
Diego Alvarez <diegoah905@gmail.com>
2021-06-11 00:00:16 -0500

    ecmascript6

--- 01-fundamentos/assets/js/tips-return.js
+++ 01-fundamentos/assets/js/tips-return.js
@@ -1,12 +1,17 @@
-tunction createPersona(nombre, apellido) {
-    //objeto literal
-    return {
-        nombre: nombre,
-        apellido: apellido
+// tunction createPersona(nombre, apellido) {
+//     //objeto literal
+//     return {
+//         nombre: nombre,
+//         apellido: apellido
 
-    }
+//     }
 
+// }
+
+const createPersona = (nombre, apellido) {
+    return {nombre, apellido}
 }
 
 const persona = createPersona('Alejo', 'Alvarez');
 console.log(persona);
+

ccff7c6451059bab466bb5a502d49739ecfe02e2
Diego Alvarez <diegoah905@gmail.com>
2021-06-21 23:37:58 -0500

    Tips sobre Javscript

---
+++ 01-fundamentos/assets/js/protip-retorno.js
@@ -0,0 +1,44 @@
+// function crearPersona(nombre, apellido) {
+//     return {
+//         nombre,
+//         apellido
+//     }
+// }
+ 
+const crearPersona = (nombre, apellido) => ({nombre, apellido}); 
+
+const persona = crearPersona( 'Diego', 'Alvarez');
+console.log(persona);
+
+function imprimeArgumentos() {
+    console.log( arguments );
+}
+
+const imprimeArgumentos2 = (edad, ...args) => { 
+    // console.log({edad, args});
+    return args;
+}
+//asignar variable a cada valor
+const [casado, vivo, nombre, saludo] = imprimeArgumentos2(10, true, false, 'Fernando', 'hola');
+console.log({casado, vivo, nombre, saludo});
+
+//asignar nuevos valores
+const {apellid: nuevoApellido} = crearPersona2('fernando', 'alvarez');
+console.log({nuevoApellido});
+
+const tony = {
+    nombre: 'tony stark',
+    codeName: 'ironman',
+    vivo: false,
+    edad: 40,
+    trajes: ['mark 1', 'mark V', 'hulkbuster']
+}
+
+const imprimePropiedades = ({nombre, codeName, vivo, edad = 14, trajes}) => {
+    console.log({nombre});
+    console.log({codeName});
+    console.log({vivo});
+    console.log({edad});
+    console.log({trajes});
+}
+imprimePropiedades(tony);

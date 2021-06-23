c1fe3ccbed094c056a61b305912e09c0c411e9bd
Diego Alvarez <diegoah905@gmail.com>
2021-06-22 23:50:28 -0500

    terminando con valor referencia y romperla

--- 01-fundamentos/assets/js/valor-referencia.js
+++ 01-fundamentos/assets/js/valor-referencia.js
@@ -5,6 +5,8 @@ let b = a;
 console.log({a, b});
 
 let juan = {nombre: 'Juan'};
+//separar la referencia y romperarla  
+//operador spread 
 let ana = {...juan};
 ana.nombre = 'Ana';
 
@@ -17,3 +19,20 @@ const cambiaNombre = (persona) => {
 
 let peter = { nombre: 'peter'};
 let tony = cambiaNombre(peter);
+
+console.log({peter, tony});
+
+//arreglos 
+const frutas = ['manzana', 'pera', 'banano'];
+
+console.time('slice');
+const otrasFrutas = frutas.slice();
+console.timeEnd('slice');
+
+console.time('spread');
+const otrasFrutas = [...frutas];
+console.timeEnd('spread');
+
+otrasFrutas.push('mango');
+
+console.table({ frutas, otrasFrutas});

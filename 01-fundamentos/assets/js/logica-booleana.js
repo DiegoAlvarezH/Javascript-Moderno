5f946727417b793a0cc8e00deba87e33b9580c86
Diego Alvarez <diegoah905@gmail.com>
2021-06-28 23:56:43 -0500

    Logica booleana

---
+++ 01-fundamentos/assets/js/logica-booleana.js
@@ -0,0 +1,13 @@
+
+const regresaTrue = () => {
+    console.log('Regresa true');
+    return true;
+}
+
+
+const regresaFalse = () => {
+    console.log('Regresa false');
+    return false;
+}
+
+//es una manera que tenemos para trabajar con valores boleanos

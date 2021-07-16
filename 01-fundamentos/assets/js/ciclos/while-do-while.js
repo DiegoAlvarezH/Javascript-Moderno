d6bd9545e44af2701749e91c03cc11c6a0124836
Diego Alvarez <diegoah905@gmail.com>
2021-07-16 10:13:46 -0500

    ciclo while-do-while

---
+++ 01-fundamentos/assets/js/ciclos/while-do-while.js
@@ -0,0 +1,38 @@
+
+const carros =['Ford', 'Mazada', 'Honda', 'Toyota'];
+
+let i = 0;
+//se ejecuta siempre y cuando lo que entre o la condicion sea true 
+
+// while(i < carros.length) {
+//     console.log(carros[i]);
+//     // i = i + i;
+//     // i += 2 
+//     i++
+// }
+
+//las condiciones falsas que pueden hacer que nunca se ejecuten 
+// undefined
+//null 
+//false
+
+console.warn('While')
+
+// while(carros[i]){
+//     if( i === 1 ){
+//         // break;
+//         i++
+//         continue;
+//     }
+//     console.log(carros[i]);
+//     i++
+// }
+
+//el do while va a ejecutar el bloque interno solamente una vez
+
+// console.warn('Do while');
+// let j = 0;
+// do{
+//     console.log(carros[j]);
+//     j++
+// }while(carros(j));
